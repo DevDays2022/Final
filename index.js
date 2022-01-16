2dfbc1bbd36255c5f13bbe69c48ba09ba86a2e0c
IKJYOT-53 <ikjyot.cse1@gmail.com>
2022-01-16 19:09:02 +0530

    contact form functionality

--- index.js
+++ index.js
@@ -63,7 +63,17 @@ const draw = () => {
 setInterval(draw, 30);
 setInterval(countdown,1000);
 
-var form = document.querySelector('form');
-form.addEventListener('onsubmit', () => {
-    alert("meow")
-})
+function sendMail(params){
+    var tempParams= {
+        user_name:document.getElementById("name").value,
+        user_subj:document.getElementById("subj").value,
+        user_email:document.getElementById("email").value,
+        message:document.getElementById("message").value
+    };
+    emailjs.send('service_y66vpvr','template_ba0apmq',tempParams).then(function(res){
+        console.log(res.status);
+        if(res.status == 200){
+            document.getElementById("response").innerText="Message Sent";
+        }
+    })
+}
